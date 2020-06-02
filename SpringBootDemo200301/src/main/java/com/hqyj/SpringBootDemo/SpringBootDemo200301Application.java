845199739110e9339db44d1e4ccf1ed876e25829
Administrator <Administrator@IMEX8H3ER0Q16W1>
2020-06-02 15:38:59 +0800

    "2020-06-02:搭建框架,实现第一个接口"

---
+++ SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/SpringBootDemo200301Application.java
@@ -0,0 +1,16 @@
+package com.hqyj.SpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootDemo200301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo200301Application.class, args);
+	}
+	
+
+}
